28339de14a3d82bdebdd2e52cbf56432cdc2825d
Lihess <she6569@gmail.com>
2020-08-27 01:22:42 +0900

    Chart : selector 추가

--- BodyFlow/app/pages/Chart.js
+++ BodyFlow/app/pages/Chart.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { NavigationService } from '../router/service';
 import { SafeAreaView, TouchableOpacity, View, Text, StatusBar } from 'react-native';
+import SwitchSelector from 'react-native-switch-selector';
 import { Ionicons } from '@expo/vector-icons'; 
 import { common } from '../styles/Common.Style';
 import styles from '../styles/chart/Chart.style';
@@ -13,8 +14,10 @@ export default class Chart extends React.Component {
     static navigationOptions = { headerShown : false };
 
     state = {
-        part : this.props.navigation.state.params.part,
-        modalVisible : false
+        part : this.props.navigation.state.params.part ? this.props.navigation.state.params.part : '어깨',
+        modalVisible : false,
+        unit : 'cm',
+        period : 'lately'
     }
 
     // visible 값 변경
@@ -23,8 +26,18 @@ export default class Chart extends React.Component {
     }
 
     // part 변경
-    onChangPart = (part) => {
-        this.setState({part : part})
+    onChangPart = (selectPart) => {
+        this.setState({part : selectPart})
+    }
+
+    // 단위 선택 시, 해당 단위로 state 값 변경
+    onSelectUnit = (value) => {
+        value == 0 ? this.setState({unit : 'cm'}) : this.setState({unit : 'in'});
+    }
+
+    // 기간 선택 시, 해당 기간으로 state 값 변경
+    onSelectPeriod = (value) => {
+        value == 0 ? this.setState({period : 'lately'}) : this.setState({period : 'total'})
     }
 
     render(){
@@ -39,7 +52,40 @@ export default class Chart extends React.Component {
                     </TouchableOpacity> 
                 </View>
 
-                <ChartByPart part={this.state.part}/>
+                <View style={styles.selectorBox}>
+                    <SwitchSelector 
+                            style={styles.switch}
+                            options={[
+                                {label : '최근', value : '0'},
+                                {label : '전체', value : '1'}
+                            ]} 
+                            initial={0} 
+                            buttonColor={'#c4c4c4'}
+                            borderRadius={5.5}
+                            height={30}
+                            alignItems={'center'}
+                            textStyle={styles.switchFont}
+                            selectedTextStyle={styles.switchFont}
+                            onPress={value => this.onSelectPeriod(value)} />
+
+                    { this.state.part != '체중' ?
+                        <SwitchSelector 
+                            style={styles.switch}
+                            options={[
+                                {label : 'cm', value : '0'},
+                                {label : 'inch', value : '1'}
+                            ]} 
+                            initial={0} 
+                            buttonColor={'#c4c4c4'}
+                            borderRadius={5.5}
+                            height={30}
+                            alignItems={'center'}
+                            textStyle={styles.switchFont}
+                            selectedTextStyle={styles.switchFont}
+                            onPress={value => this.onSelectUnit(value)} /> : null }
+                </View>
+
+                <ChartByPart part={this.state.part} period={this.state.period}/>
 
                 <PartPicker 
                     visible={this.state.modalVisiable} 
