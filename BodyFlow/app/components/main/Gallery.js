3f8cd0ec2606a67c0060a55302978c149261265b
Lihess <she6569@gmail.com>
2020-09-03 23:14:49 +0900

    Gallery : s3 연결, DB에 이미지 경로 저장 로직 작성

--- BodyFlow/app/components/main/Gallery.js
+++ BodyFlow/app/components/main/Gallery.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
-import { View, TouchableOpacity,Image, Text } from 'react-native';
+import { View, TouchableOpacity, Image, Text } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 import styles from '../../styles/main/Gallery.Style'
-
+import {createPhoto} from '../../backend/Create'
 // https://dingcodingco.tistory.com/14
 // https://docs.amplify.aws/lib/storage/getting-started/q/platform/js#manual-setup-import-storage-bucket
 import Amplify, { Storage } from 'aws-amplify';
@@ -24,67 +24,63 @@ Amplify.configure({
 });
 
 export default class Gallery extends React.Component {
-    state = {
-        modalVisible : false,
-        image: null,
+    componentDidMount = async() => {
+        await Permissions.askAsync(Permissions.CAMERA_ROLL);
     }
-    
-    getPermissionAsync = async () => {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-        if (status !== 'granted') {
-          alert('Sorry, we need camera roll permissions to make this work!');
-        }
-    };
 
-    _pickImage = async () => {
-      await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    // 파일 선택
+    pickImage = async () => {
+        await Permissions.askAsync(Permissions.CAMERA_ROLL);
         try {
-          let result = await ImagePicker.launchImageLibraryAsync({
-            base64 : true,
-            allowsEditing: false
-          });
-          if (!result.cancelled) {
-            this.setState({ image: result.uri });
-            this.updateImage();
-          }
-    
-          console.log(result);
+            let result = await ImagePicker.launchImageLibraryAsync({
+              allowsEditing: false
+            });
+            if (!result.cancelled) {
+              this.updateImage(result.uri);
+            }
         } catch (E) {
-          console.log(E);
+            console.log(E);
         }
       };
-    
-    updateImage = async() => {
+
+    // 이미지를 s3에 저장하고 DB에 photo 객체 생성
+    updateImage = async(uri) => {
         try {
-          console.log(this.state.image)
-            fetch(this.state.image).then(response => {
-              response.blob().then(blob => {
-                Storage.put(`images/7.png`, blob, {
-                  contentType: 'image/png',
-             }).then(data => console.log(data))
-             .catch(err => console.log(err))
-              })
+            const fileName = guid()
+            const fileExtension = uri.substr(uri.lastIndexOf('.') + 1)
+
+            fetch(uri).then(response => {
+                response.blob().then(blob => {
+                    Storage.put(`images/${fileName}.${fileExtension}`, blob, {
+                        contentType: `image/${fileExtension}`,
+                    })
+                    .then(() => {createPhoto(`https://body-flow.s3.ap-northeast-2.amazonaws.com/public/images/${fileName}.${fileExtension}`)})
+                    .catch(err => console.log(err))
+                })
             })
-            
         } catch (err) {
             console.log(err)
         }
     }
 
-    toggleVisible = () => {
-        this.setState({modalVisible : !this.state.modalVisible})
-    }
 //https://steemit.com/kr/@anpigon/react-native-flatlist-infinite-scroll-pull-down-refresh
 // 무한 스크롤
     render(){
         return (
             <View style={styles.container}>
-              <Image style={{width:'60%', backgroundColor : 'red'}} source={{uri :'https://body-flow.s3.ap-northeast-2.amazonaws.com/public/images/1.png'}}></Image>
                 <Text style={styles.placeholder}> 당신의 몸을 기록해보세요 </Text>
-                <TouchableOpacity style={styles.button} onPress={this._pickImage}>
+                <TouchableOpacity style={styles.button} onPress={this.pickImage}>
                     <MaterialCommunityIcons name="camera-plus" size={30} color="white" />
-                </TouchableOpacity>
+                </TouchableOpacity> 
             </View>
         );
     }
+}
+
+// 파일 이름을 유니크하게 하기 위한 함수
+function guid() {
+        function _s4() {
+            return ((1 + Math.random()) * 0x10000 | 0).toString(16).substring(1);
+    }
+    return _s4() + _s4() + '-' + _s4() + '-' + _s4() + '-' + _s4() + '-' + _s4() + _s4() + _s4();
 }
